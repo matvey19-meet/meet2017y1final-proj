811fbbcb84c0d559235d0407681035ab091fd075
shiraz19-meet <shiraz19@meet.mit.edu>
2017-08-07 17:02:42 +0300

    Stopping here

--- blocks.py
+++ blocks.py
@@ -1,6 +1,8 @@
 import turtle
 SIZE_X=1000
 SIZE_Y=500
+up_edge=250
+down_edge=-250
 turtle.setup(SIZE_X,SIZE_Y)
 
 turtle.hideturtle()
@@ -25,6 +27,7 @@ def make_log():
         pos_list.append(my_pos)
         new_stamp=log.stamp()
         stamp_list.append(new_stamp)
+
 
 make_log()
 
@@ -65,6 +68,45 @@ def make_log2():
         stamp_list.append(new_stamp)
 make_log2()
 
+log3=turtle.clone()
+log3.shape('square')
+log3.color('brown')
+log3.goto(200,0)
+
+def make_log3():
+    for i in range(START_LENGTH):
+        x_pos=log3.pos()[0]
+        y_pos=log3.pos()[1]
+        x_pos+=SQUARE_SIZE
+        my_pos=(x_pos,y_pos)
+        log3.goto(x_pos,y_pos)
+        pos_list.append(my_pos)
+        new_stamp=log3.stamp()
+        stamp_list.append(new_stamp)
+make_log3()
+
+def move_log():
+    for my_pos in pos_list:
+    #my_pos=log.pos()
+        x_pos=my_pos[0]
+        y_pos=my_pos[1]
+        log.goto(x_pos,y_pos+SQUARE_SIZE)
+        pos_list.append(my_pos)
+        new_stamp=log.stamp()
+        stamp_list.append(new_stamp)
+        old_stamp=stamp_list.pop(0)
+        log.clearstamp(old_stamp)
+        pos_list.pop(0)
+        new_pos=log.pos()
+    
         
- 
- 
+
+    TIME_STEP=10
+    turtle.ontimer(move_log,TIME_STEP)
+    
+
+move_log()
+
+    
+
+
