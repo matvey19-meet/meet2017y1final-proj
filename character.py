496743cc83c1cb7f23203d542cdfc555f5327d9a
shiraz19-meet <shiraz19@meet.mit.edu>
2017-08-09 10:30:49 +0300

    Stopping here

--- character.py
+++ character.py
@@ -7,22 +7,24 @@ pos_list = []
 
 #CREATE CHARACTER SHAPE
 
-turtle.register_shape("character.gif")
+turtle.register_shape("characterleft.gif")
+turtle.register_shape("characterright.gif")
 character= turtle.clone()
-character.shape("character.gif")
+character.shape("characterright.gif")
+character.penup()
 turtle.hideturtle()
 
 #CONSTRUCT CHARACTER
 
 start_pos = character.pos()
 
-UP_ARROW = "Up"
-LEFT_ARROW = "Left"   
-DOWN_ARROW = "Down"
-RIGHT_ARROW = "Right"
+UP_ARROW = "w"
+LEFT_ARROW = "a"   
+DOWN_ARROW = "s"
+RIGHT_ARROW = "d"
 
 TIME_STEP = 100
-speed = 50
+speed = 10
 
 SPACEBAR = "space"
 
@@ -30,6 +32,9 @@ UP = 0
 LEFT = 1
 DOWN = 2
 RIGHT = 3
+
+num_1=3
+num_2= 5
 
 
 direction = UP
@@ -39,62 +44,105 @@ def up():
     global direction
     direction = UP
     move_char()
-    print("You pressed the up key")
+    #print("You pressed the up key")
     
 def left():
     global direction, horizontal_direction
     direction = LEFT
     horizontal_direction = LEFT
+    character.shape("characterleft.gif")
     move_char()
-    print("You pressed the left key")
-
+    #print("You pressed the left key")
+    
 def down():
     global direction
     direction = DOWN
     move_char()
-    print("You pressed the down key")
-
+    #print("You pressed the down key")
+    
 def right():
     global direction, horizontal_direction
     direction=RIGHT
     horizontal_direction = RIGHT
+    character.shape("characterright.gif")
     move_char()
-    print("You pressed the right key")
+    #print("You pressed the right key")
 
-    
-def jump():
+turtle.hideturtle()
+#______________________________________________________________________________________________|    
+def jump_right():                                                                              
   
     turtle.ontimer(character.goto(character.pos()[0], character.pos()[1]),speed)
-    turtle.ontimer(character.goto(character.pos()[0]+10, character.pos()[1]+ 10),speed)
-    turtle.ontimer(character.goto(character.pos()[0]+10, character.pos()[1]+ 10),speed)
-    turtle.ontimer(character.goto(character.pos()[0]+10, character.pos()[1]+ 10),speed)
-    turtle.ontimer(character.goto(character.pos()[0]+10, character.pos()[1]+ 10),speed)
-    turtle.ontimer(character.goto(character.pos()[0]+10, character.pos()[1]+ 10),speed)
-    turtle.ontimer(character.goto(character.pos()[0]+10, character.pos()[1]+ 10),speed)
-    turtle.ontimer(character.goto(character.pos()[0]+10, character.pos()[1]+ 10),speed)
-    turtle.ontimer(character.goto(character.pos()[0]+10, character.pos()[1]+ 10),speed)
-    turtle.ontimer(character.goto(character.pos()[0]+10, character.pos()[1]+ 10),speed)
-    turtle.ontimer(character.goto(character.pos()[0]+10, character.pos()[1]+ 10),speed)
-    turtle.ontimer(character.goto(character.pos()[0]+10, character.pos()[1]+ 10),speed)
-    turtle.ontimer(character.goto(character.pos()[0]+10, character.pos()[1]+ 10),speed)
-    turtle.ontimer(character.goto(character.pos()[0]+10, character.pos()[1]+ 10),speed)
-    turtle.ontimer(character.goto(character.pos()[0]+10, character.pos()[1]+ 10),speed)
-    turtle.ontimer(character.goto(character.pos()[0]+10, character.pos()[1]- 10),speed)
-    turtle.ontimer(character.goto(character.pos()[0]+10, character.pos()[1]- 10),speed)
-    turtle.ontimer(character.goto(character.pos()[0]+10, character.pos()[1]- 10),speed)
-    turtle.ontimer(character.goto(character.pos()[0]+10, character.pos()[1]- 10),speed)
-    turtle.ontimer(character.goto(character.pos()[0]+10, character.pos()[1]- 10),speed)
-    turtle.ontimer(character.goto(character.pos()[0]+10, character.pos()[1]- 10),speed)
-    turtle.ontimer(character.goto(character.pos()[0]+10, character.pos()[1]- 10),speed)
-    turtle.ontimer(character.goto(character.pos()[0]+10, character.pos()[1]- 10),speed)
-    turtle.ontimer(character.goto(character.pos()[0]+10, character.pos()[1]- 10),speed)
-    turtle.ontimer(character.goto(character.pos()[0]+10, character.pos()[1]- 10),speed)
-    turtle.ontimer(character.goto(character.pos()[0]+10, character.pos()[1]- 10),speed)
-    turtle.ontimer(character.goto(character.pos()[0]+10, character.pos()[1]- 10),speed)
-    turtle.ontimer(character.goto(character.pos()[0]+10, character.pos()[1]- 10),speed)
-    turtle.ontimer(character.goto(character.pos()[0]+10, character.pos()[1]- 10),speed)
+    turtle.ontimer(character.goto(character.pos()[0]+num_1, character.pos()[1]+ num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]+num_1, character.pos()[1]+ num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]+num_1, character.pos()[1]+ num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]+num_1, character.pos()[1]+ num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]+num_1, character.pos()[1]+ num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]+num_1, character.pos()[1]+ num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]+num_1, character.pos()[1]+ num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]+num_1, character.pos()[1]+ num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]+num_1, character.pos()[1]+ num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]+num_1, character.pos()[1]+ num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]+num_1, character.pos()[1]+ num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]+num_1, character.pos()[1]+ num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]+num_1, character.pos()[1]+ 2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]+num_1, character.pos()[1]+ 1),speed)
+    turtle.ontimer(character.goto(character.pos()[0]+num_1, character.pos()[1]- 1),speed)
+    turtle.ontimer(character.goto(character.pos()[0]+num_1, character.pos()[1]- 2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]+num_1, character.pos()[1]- num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]+num_1, character.pos()[1]- num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]+num_1, character.pos()[1]- num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]+num_1, character.pos()[1]- num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]+num_1, character.pos()[1]- num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]+num_1, character.pos()[1]- num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]+num_1, character.pos()[1]- num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]+num_1, character.pos()[1]- num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]+num_1, character.pos()[1]- num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]+num_1, character.pos()[1]- num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]+num_1, character.pos()[1]- num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]+num_1, character.pos()[1]- num_2),speed)
 
+def jump_left():
+  
+    turtle.ontimer(character.goto(character.pos()[0], character.pos()[1]),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]+ num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]+ num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]+ num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]+ num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]+ num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]+ num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]+ num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]+ num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]+ num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]+ num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]+ num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]+ num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]+ 4),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]+ 3),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]+ 2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]+ 1),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]- 1),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]- 2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]- 3),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]- 4),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]- num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]- num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]- num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]- num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]- num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]- num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]- num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]- num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]- num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]- num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]- num_2),speed)
+    turtle.ontimer(character.goto(character.pos()[0]-num_1, character.pos()[1]- num_2),speed)
 
+def jump():
+    if horizontal_direction==LEFT:
+        return(jump_left())
+    elif horizontal_direction==RIGHT:
+        return(jump_right())
 
 turtle.onkeypress(up,UP_ARROW)
 turtle.onkeypress(down,DOWN_ARROW)
